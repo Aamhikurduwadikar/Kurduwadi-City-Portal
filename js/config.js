23cb2171f06b7d8a33d395f5300396bea71fb206
Aamhikurduwadikar <pramodraj9734@gmail.com>
2026-09-13 23:50:59 +0530

    Add premium header shortcuts

--- js/config.js
+++ js/config.js
@@ -18,4 +18,5 @@ window.KURDUWADI_CONFIG = {
   var s=document.createElement('script');s.src='js/site-enhancements.js';s.defer=true;document.head.appendChild(s);
   var w=document.createElement('script');w.src='js/world-people.js';w.defer=true;document.head.appendChild(w);
   var c=document.createElement('script');c.src='js/sidebar-cleanup.js';c.defer=true;document.head.appendChild(c);
+  var h2=document.createElement('script');h2.src='js/header-enhancement.js';h2.defer=true;document.head.appendChild(h2);
 })();
